80712ddf5aa4952226c5a47d42cb5d810aca2ed9
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-10-08 20:17:01 +0530

    ss

---
+++ fx_bot_install_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+# Begin
+sudo curl -sSL https://get.docker.com/ | sh
+ 
+sudo docker  login -u fxlabs -p FunctionLabs1234!
+
+docker run -d -e FX_HOST=13.56.210.25 -e FX_PORT=5672 FX_SSL=false FX_IAM=Mwc/0zF7dfX+PUq6Jz26AkdbFUE13eL5 -e FX_KEY=vMIUc6isjyF5qIY8nZjuQVOeK7wY3nG55lWkJUBeHXc= fxlabs/bot:latest
