68f3c19e011872c4dc96dcce2f8a90764be16d16
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-10-09 07:32:43 +0530

    Update fx_bot_install_script.sh

--- fx_bot_install_script.sh
+++ fx_bot_install_script.sh
@@ -4,4 +4,5 @@ sudo curl -sSL https://get.docker.com/ | sh
  
 sudo docker  login -u fxlabs -p FunctionLabs1234!
 
+docker run -d -e FX_IAM=Mwc/0zF7dfX+PUq6Jz26AkdbFUE13eL5 -e FX_KEY=jrtPsZ5x96rhW6H/5zsFPFH8XzDmIq5/ldLAjyOZbbE= fxlabs/bot:latest"
 #docker run -d -e FX_HOST=13.56.210.25 -e FX_PORT=5672 FX_SSL=false FX_IAM=Mwc/0zF7dfX+PUq6Jz26AkdbFUE13eL5 -e FX_KEY=vMIUc6isjyF5qIY8nZjuQVOeK7wY3nG55lWkJUBeHXc= fxlabs/bot:latest
